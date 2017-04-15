220496c0670633c7b057262358aee2eff0c13247
tyler-whited <tyler.whited@domo.com>
2017-04-14 18:26:06 -0600

    Flatten array of random numbers. Add TS sugar to maini.

--- main.ts
+++ main.ts
@@ -7,12 +7,12 @@ let scrollLoader:ScrollLoader;
 
 main();
 
-function main(){
+function main():void{
   scrollLoader = new ScrollLoader(PAGE_SIZE);
   getMore();
 }
 
-function getMore(){
+function getMore():void{
   console.log('\nGetting more data:');
   scrollLoader.next().then(res=>{
     if(!res.done){
